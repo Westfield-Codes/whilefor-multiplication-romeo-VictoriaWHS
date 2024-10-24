481f6cc7a1f6a7c9a2734373ee1f171c0f89bf7f
VictoriaWHS <victoria202975@student-schoolsofwestfield.org>
2024-10-24 16:12:33 +0000

    done

--- done.js
+++ done.js
@@ -41,3 +41,36 @@
 
 
 /* Prompt with Conditional */
+
+//10 /24/2024//
+
+main();
+
+/* Multiplication Quiz */
+// Create 2 variables, a & b, and set to random integers between 3 and 9
+// Display them as the equation:  a * b = ??
+// Convert this display (same line, not new) to a question and store input in a variable called entry
+// Set a variable product to be the product of a and b
+// Say "Correct!" if the entry and product match, "Incorrect!" if not.
+// Embed the submission and feedback in While loop that continues until entry matches either product, or = "q".
+// Embed all this code in a For loop and run it 5 times.
+//math.floor, gets rid of decimals.  Math random generates number and times 3 = 2//
+function quizQuestion(question){
+    let a = Math.floor(Math.random()*7)+3; 
+    let b = Math.floor(Math.random()*7)+3;
+    let product = a*b;
+    let entry = 0;
+    while (entry != product && entry != "q"){
+        entry= prompt("question " + question + ": what is " + a + " * " + b + " ?");
+        if (entry == a *b ) alert("correct.")
+            else alert("incorrect");
+        
+    }
+}
+    function main() {
+        for (let question =1; question <=5; question++){
+        quizQuestion(question);
+    
+    }
+    
+}
