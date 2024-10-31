69d50a9dbf6546fcafd7255b77e3a153be15da06
VictoriaWHS <victoria202975@student-schoolsofwestfield.org>
2024-10-31 15:54:13 +0000

    done

--- kata.js
+++ kata.js
@@ -1,8 +1,61 @@
-/* Multiplication Quiz */
-// Create 2 variables, a & b, and set to ne random integers between 3 and 9
-// Display them as the equation:  a * b = ??
-// Convert this display (same line, not new) to a question and store input in a variable called entry
-// Set a variable product to be the product of a and b
-// Say "Correct!" if the entry and product match, "Incorrect!" if not.
-// Embed the submission and feedback in While loop that continues until entry matches either product, or = "q".
-// Embed all this code in a For loop and run it 5 times.
+
+/* Multiplication Quiz
+ * This program should ask five multiplication questions, displaying which question
+ * is being asked, and counting the number of equations with at least one error.
+
+
+
+
+/* Setup */
+// Define a function called main and then call it up top.
+// Define a function called askFive and call it from inside main().
+// Define a function called askQuestion and call it from inside askFive() as askQuestion(1);
+
+
+/* STAGE 1:ASK A QUESTION */
+
+
+/* askQuestion asks a multiplication question, returns 1 if incorrect
+ * @param: question (integer 1-5)
+ * @return: integer (0 or 1)
+ */
+// Create a variable named a, and set to a random integer between 3 and 9.
+// Create a variable named b, and set to a random integer between 3 and 9.
+// Create a variable called product, set as a * b.
+// Create a variable called equation, set as the complex string a * b = ?.
+// Create a variable called answer, ask user for it by prompting with equation.
+
+// Display "Correct!" and return 0 if the answer and product match.
+// Otherwise, display "Incorrect!" and return 1.
+
+
+/* TEST BEFORE CONTINUING TO STAGE TWO! */
+
+
+/* STAGE 1:ASK 5 QUESTIONS */
+
+
+/* askFive calls askQuestion five times, counting and returning number wrong
+ * @param: none
+ * @return: score (0-5)
+ */
+// Create a variable score, set to 0
+// Write a for loop with question as the index, values 1 to 5
+// Call askQuestion in the loop, with question as argument
+// Add the returned value of askQuestion to score each time you call it (same line)
+// return score after loop finishes
+
+
+/* TEST BEFORE CONTINUING TO STAGE THREE! */
+
+
+/* STAGE 3: MULTIPLE TRIES (CHALLENGE!) */
+
+
+/* Refactor askQuestion per whileQuestions askFive calls askQuestion five times, counting and returning number wrong
+ * Use the whileQuestions tab in the flowchart to guide you.
+ * @param: none
+ * @return: score (0-5)
+ */
+
+
--- script.js
+++ script.js
@@ -1,41 +1,72 @@
-main();
 
-/* Multiplication Quiz */
-// Create 2 variables, a & b, and set to random integers between 3 and 9
-// Display them as the equation:  a * b = ??
-// Convert this display (same line, not new) to a question and store input in a variable called entry
-// Set a variable product to be the product of a and b
-// Say "Correct!" if the entry and product match, "Incorrect!" if not.
-// Embed the submission and feedback in While loop that continues until entry matches either product, or = "q".
-// Embed all this code in a For loop and run it 5 times.
-//math.floor, gets rid of decimals.  Math random generates number and times 3 = 2//
-function quizQuestion(question){
-    let a = Math.floor(Math.random()*7)+3; 
+/* Multiplication Quiz 
+ * This program should ask five multiplication questions, displaying which question
+ * is being asked, and counting the number of equations with at least one error.
+ * FLOWCHART: https://lucid.app/lucidchart/5a3164fd-459f-494d-9cae-b4a6be593b13/view
+ */
+main()
+function main() {
+    let score = askFive();
+    if (score == 0) alert("Perfect!");
+    else alert("You had "+score+" errors");
+}
+/* Setup */
+// Define a function called main and then call it up top.
+// Define a function called askFive and call it from inside main().
+// Define a function called askQuestion and call it from inside askFive() as askQuestion(1);
+
+/* STAGE 1:ASK A QUESTION */
+
+/* askQuestion asks a multiplication question, returns 1 if incorrect
+ * @param: question (integer 1-5)
+ * @return: integer (0 or 1)
+ */
+function askQuestion(question){
+    let a = Math.floor(Math.random()*7)+3;
     let b = Math.floor(Math.random()*7)+3;
-    let equation = a*b;
-    let entry = 0;
-    while (entry != equation && entry != "q"){
-        entry= prompt("question " + question + ": what is " + a + " * " + b + " ?");
-        if (entry == a *b ) alert("correct.")
-            else alert("incorrect");
-        //must return to 1 but idk how//
+    let product = a*b;
+    let equation = a + " * " + b + " = ?";
+    let answer = prompt(equation);
+    if (answer == "q"){
+        alert("Quitter!");
+        return 2;
     }
+    else if (answer == product){
+       alert("Correct!");
+       return 0;
+    } 
+    else {
+        alert("incorrect");
+        return 1;   
+    } 
 }
-    function main() {
-        for (let question =1; question <=5; question++){
-        quizQuestion(question);
-    
-    }
-    
-}
-function askFive(){
-    askQuestion();
-    askQuestion();
-    askQuestion();
-    askQuestion();
-    askQuestion();
+/* TEST BEFORE CONTINUING TO STAGE TWO! */
 
+/* STAGE 1:ASK 5 QUESTIONS */
+
+/* askFive calls askQuestion five times, counting and returning number wrong
+ * @param: none
+ * @return: score (0-5)
+ */
+function askFive() {
+    let score = 0;
+    for (let question = 1; question <= 5; question++) {
+        score += askQuestion(question);
+    }
+    return score;
 }
-var score =0;
-for (let question=1; question<6; question = question +//???)//
-    
+// Create a variable score, set to 0
+// Write a for loop with question as the index, values 1 to 5
+// Call askQuestion in the loop, with question as argument
+// Add the returned value of askQuestion to score each time you call it (same line)
+// return score after loop finishes
+
+/* TEST BEFORE CONTINUING TO STAGE THREE! */
+
+/* STAGE 3: MULTIPLE TRIES (CHALLENGE!) */
+
+/* Refactor askQuestion per whileQuestions askFive calls askQuestion five times, counting and returning number wrong
+ * Use the whileQuestions tab in the flowchart to guide you. 
+ * @param: none
+ * @return: score (0-5)
+ */
